4f72c4946040635d698e71537e021bbeb70360d3
Chamsae7 <dididd0112@gmail.com>
2024-06-13 21:40:36 +0900

    쓰레기 피하기
    슈팅 게임

---
+++ Scripts/Weapon.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 10f;
+
+    public float damage = 1f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 1f);
+        transform.rotation = Quaternion.Euler(0, 0, 90);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.up * moveSpeed * Time.deltaTime;
+    }
+}
